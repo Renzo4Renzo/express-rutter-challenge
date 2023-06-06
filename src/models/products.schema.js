bce323a98798ccc9ce0a10f8c16ef018c01601a6
Renzo Diaz <renzodiaz554@gmail.com>
2023-06-05 23:43:09 -0500

    GET /products/fetch completed

--- src/models/products.schema.js
+++ src/models/products.schema.js
@@ -1,34 +1,21 @@
-import { v4 as uuidv4 } from "uuid";
+import mongoose from "mongoose";
 
-import productModel from "./products.model.js";
+const productsSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+    },
+    platform_id: {
+      type: Number,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: false, versionKey: false }
+);
 
-export async function fetchProducts() {}
-
-export async function getProducts(filters) {
-  const { id, platform_id, title } = filters;
-  const maxLimit = Number(process.env.DEFAULT_PAGINATION_LIMIT);
-  const limit =
-    filters?.limit && Number(filters.limit) > 0 && Number(filters.limit) < maxLimit ? Number(filters.limit) : maxLimit;
-  const page = filters?.page && Number(filters.page) > 0 ? Number(filters.page) : 1;
-
-  const productQuery = {
-    ...(id && { id }),
-    ...(platform_id && { platform_id }),
-    ...(title && { title: { $regex: new RegExp(title, "i") } }),
-  };
-
-  const productList = await productModel.find(productQuery, null, {
-    limit: Number(limit),
-    skip: Number(limit) * (Number(page) - 1),
-    sort: { title: 1 },
-  });
-  const totalCount = await productModel.find(productQuery).count();
-
-  return {
-    products: productList,
-    page: page,
-    pageSize: limit,
-    totalPages: Math.ceil(limit ? totalCount / limit : 1),
-    totalCount: totalCount,
-  };
-}
+export default mongoose.model("Product", productsSchema);
--- src/routes/products/products.controller.js
+++ src/routes/products/products.controller.js
@@ -1,11 +1,58 @@
-import { fetchProducts, getProducts } from "../../models/products.schema.js";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
+import { storeProducts, getProducts } from "../../models/products.model.js";
+
+const rutterShopifyURL = process.env.RUTTER_SHOPIFY_URL;
+const token = process.env.TOKEN_RUTTER_SHOPIFY_URL;
+let fetchProductsBulkWriteResult;
 
 export async function httpFetchProducts(req, res) {
   try {
-    return res.status(200).json({ message: "Hi!" });
+    resetFetchProductsBulkWriteResult();
+    let { limit } = req.query;
+    if (!limit || limit > 50) limit = 50;
+    await fetchProductsFromShopify(`${rutterShopifyURL}/products.json?limit=${limit}`, token);
+    const response = {
+      message: "Products fetched successfully from Rutter Shopify!",
+      result: fetchProductsBulkWriteResult,
+    };
+    return res.status(200).json(response);
   } catch (error) {
-    return res.status(400).json({ message: "Hi!" });
+    return res.status(500).json({ message: "There was an error fetching the products!", errors: error.message });
   }
+}
+
+async function fetchProductsFromShopify(shopifyURL, shopifyToken) {
+  const { data, headers } = await axios.get(shopifyURL, {
+    headers: {
+      "X-Shopify-Access-Token": shopifyToken,
+    },
+  });
+  await storeFetchedProducts(data);
+  if (headers.link && headers.link.includes(`rel="next"`)) {
+    let startIndex;
+    if (headers.link.includes(`rel="previous",`)) {
+      startIndex = headers.link.indexOf(`rel="previous"`) + 15;
+    }
+    const shopifyLink = headers.link
+      .slice(startIndex)
+      .split(";")[0]
+      .replace(/[\<\>]/g, "");
+
+    await fetchProductsFromShopify(shopifyLink, shopifyToken);
+  }
+}
+
+async function storeFetchedProducts(fetchedData) {
+  const transformedProducts = fetchedData.products.map((product) => {
+    return {
+      id: uuidv4(),
+      platform_id: product.id,
+      title: product.title,
+    };
+  });
+  const response = await storeProducts(transformedProducts);
+  updateFetchProductsBulkWriteResult(response);
 }
 
 export async function httpGetProducts(req, res) {
@@ -22,4 +69,24 @@ export async function httpGetProducts(req, res) {
   } catch (error) {
     return res.status(400).json({ message: "There was an error retrieving the products!", errors: error.message });
   }
+}
+
+function updateFetchProductsBulkWriteResult(response) {
+  fetchProductsBulkWriteResult = {
+    insertedCount: fetchProductsBulkWriteResult.insertedCount + response.insertedCount,
+    matchedCount: fetchProductsBulkWriteResult.matchedCount + response.matchedCount,
+    modifiedCount: fetchProductsBulkWriteResult.modifiedCount + response.modifiedCount,
+    deletedCount: fetchProductsBulkWriteResult.deletedCount + response.deletedCount,
+    upsertedCount: fetchProductsBulkWriteResult.upsertedCount + response.upsertedCount,
+  };
+}
+
+function resetFetchProductsBulkWriteResult() {
+  fetchProductsBulkWriteResult = {
+    insertedCount: 0,
+    matchedCount: 0,
+    modifiedCount: 0,
+    deletedCount: 0,
+    upsertedCount: 0,
+  };
 }
